8f61a089f59529ead17d8788c8eeaf1d64f0f0f2
Sanpi <sanpi@homecomputing.fr>
2017-12-04 09:56:46 +0100

    Impl Display for Task

--- src/task.rs
+++ src/task.rs
@@ -46,3 +46,43 @@ impl ::std::str::FromStr for Task
         )
     }
 }
+
+impl ::std::fmt::Display for Task
+{
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result
+    {
+        if self.finished {
+            f.write_str("x ")?;
+        }
+
+        if self.priority < 26 {
+            let priority = (b'A' + self.priority) as char;
+
+            f.write_str(format!("({}) ", priority).as_str())?;
+        }
+
+        if let Some(finish_date) = self.finish_date {
+            f.write_str(format!("{} ", finish_date.format("%Y-%m-%d")).as_str())?;
+        }
+
+        if let Some(create_date) = self.create_date {
+            f.write_str(format!("{} ", create_date.format("%Y-%m-%d")).as_str())?;
+        }
+
+        f.write_str(self.subject.as_str())?;
+
+        if let Some(due_date) = self.due_date {
+            f.write_str(format!(" due:{}", due_date.format("%Y-%m-%d")).as_str())?;
+        }
+
+        if let Some(threshold_date) = self.threshold_date {
+            f.write_str(format!(" t:{}", threshold_date.format("%Y-%m-%d")).as_str())?;
+        }
+
+        for (key, value) in self.tags.iter() {
+            f.write_str(format!(" {}:{}", key, value).as_str())?;
+        }
+
+        Ok(())
+    }
+}
--- tests/task.rs
+++ tests/task.rs
@@ -13,3 +13,22 @@ fn from_str()
 
     assert_eq!(::todo_txt::Task::from_str(line), Ok(task));
 }
+
+#[test]
+fn display()
+{
+    let task = ::todo_txt::Task {
+        subject: "@Email SoAndSo at soandso@example.com".to_owned(),
+        priority: 1,
+        finished: true,
+        due_date: Some(::todo_txt::Date::from_ymd(2019, 2, 10)),
+        finish_date: Some(::todo_txt::Date::from_ymd(2019, 2, 15)),
+        create_date: Some(::todo_txt::Date::from_ymd(2019, 2, 5)),
+
+        .. Default::default()
+    };
+
+    let line = format!("{}", task);
+
+    assert_eq!(line, "x (B) 2019-02-15 2019-02-05 @Email SoAndSo at soandso@example.com due:2019-02-10");
+}
