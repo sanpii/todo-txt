8a076b17d316cdb0f5d58052a859c5a43eaaf169
Sanpi <sanpi@homecomputing.fr>
2017-12-04 12:13:21 +0100

    Add cargo-fuzz configuration

---
+++ fuzz/fuzzers/parse_task.rs
@@ -0,0 +1,11 @@
+#![no_main]
+
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate todo_txt;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = String::from_utf8(data.to_vec()) {
+        let _ = ::todo_txt::parser::task(&s);
+    }
+});
