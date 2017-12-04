0f240663a953ef06813a74633d4d4bb397064ffd
Sanpi <sanpi@homecomputing.fr>
2017-12-04 13:41:49 +0100

    Add simple example

---
+++ examples/simple.rs
@@ -0,0 +1,11 @@
+extern crate todo_txt;
+
+use ::std::str::FromStr;
+
+fn main()
+{
+    let line = "x (A) 2016-05-20 2016-04-30 measure space for +chapelShelving @chapel due:2016-05-30";
+    let task = ::todo_txt::Task::from_str(line);
+
+    println!("{:#?}", task);
+}
