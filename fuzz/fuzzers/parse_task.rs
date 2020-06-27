0fe2500ef8b4cbda30097d8dccba46e70b36617f
Sanpi <sanpi@homecomputing.fr>
2020-06-27 21:06:28 +0200

    [fuzz] Updates libfuzzer-sys to 0.2

--- fuzz/fuzzers/parse_task.rs
+++ fuzz/fuzzers/parse_task.rs
@@ -1,6 +1,7 @@
 #![no_main]
+use libfuzzer_sys::fuzz_target;
 
-libfuzzer_sys::fuzz_target!(|data: &[u8]| {
+fuzz_target!(|data: &[u8]| {
     if let Ok(s) = String::from_utf8(data.to_vec()) {
         let _ = todo_txt::parser::task(&s);
     }
