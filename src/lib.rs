f2e75a67ba6d270ef5a11844565b236709a1772d
Sanpi <sanpi@homecomputing.fr>
2022-03-03 15:42:38 +0100

    Adds date::today() function

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 #![warn(warnings)]
 
+pub mod date;
 pub mod parser;
 pub mod task;
 
 mod errors;
 mod priority;
 
+pub use crate::date::Date;
 pub use crate::errors::*;
 pub use crate::priority::Priority;
 pub use crate::task::Task;
-pub use chrono::NaiveDate as Date;
